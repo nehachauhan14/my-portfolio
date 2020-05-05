b696738676967c5c089a4992f1e31c289d400de8
Neha.Singh <neha.singh1@1MG-LPTP-970.local>
2020-05-05 20:03:17 +0530

    Made new repo to fix config issue

---
+++ src/components/Contact/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import "./Contact.scss";
+
+const Contact = props => {
+    return (
+        <section id="contact" className="contactSection">
+          <h1 className="contactTitle">Get in touch</h1>
+          <p className="contactSubTitle">If you wanna get in touch, talk to me about a project collaboration or just say hi,
+            <br />
+            send an email to
+            <span className="email">
+              <a href="mailto:nehachauhan79@gmail.com">nehachauhan79@gmail.com</a>
+            </span>
+            and ~let's talk.
+          </p>
+          <img src="src/assets/images/dog.gif" alt="dog lover" className="dogImg" />
+          <div className="quote">Living, learning, & leveling up one day at a time.</div>
+        </section>
+
+    )
+};
+
+export default Contact;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+  },
+  mode: 'devlopment',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(scss)$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+          options: {
+            outputPath: 'sdsds',
+          },
+        },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
