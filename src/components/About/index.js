362b174fc171699c68c14b80707234cc2f2cbd78
Neha.Singh <nehachauhan79@gmail.com>
2020-05-10 01:33:30 +0530

    Completed contact page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,10 +4,12 @@ import "./About.scss";
 const About = props => {
     return (
         <section id="about" className="aboutSection">
-            <h1 className="title">Neha Chauhan</h1>
-            <img src="src/assets/images/cartoon2.jpeg" className="cartoon" alt="me" />
-            <p className="line-1 anim-typewriter">I am a Web Developer & Dog lover... </p>
-            <div className="intro">I design and code beautifully simple things, and I love what I do.</div>
+            <div className="content">
+                <h1 className="title">Neha Chauhan</h1>
+                <img src="src/assets/images/cartoon2.jpeg" className="cartoon" alt="me" />
+                <p className="line-1 anim-typewriter">I am a Web Developer & Dog lover... </p>
+                <div className="intro">I design and code beautifully simple things, and I love what I do.</div>
+            </div>
         </section>
 
     )
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,104 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./Contact.scss";
 
-const Contact = props => {
-    return (
-        <section id="contact" className="contactSection">
-          <h1 className="contactTitle">Get in touch</h1>
-          <p className="contactSubTitle">If you wanna get in touch, talk to me about a project collaboration or just say hi,
-            <br />
-            send an email to
-            <span className="email">
-              <a href="mailto:nehachauhan79@gmail.com">nehachauhan79@gmail.com</a>
+const Contact = (props) => {
+  return (
+    <section id="contact" className="contactSection">
+      <div className="content">
+        <h1 className="contactTitle">Get in touch</h1>
+        <p className="contactSubTitle">
+          If you wanna get in touch, talk to me about a project collaboration or
+          just say hi,
+          <br />
+          send an email to
+          <span className="email">
+            <a href="mailto:nehachauhan79@gmail.com">nehachauhan79@gmail.com</a>
+          </span>
+          and ~let's talk.
+        </p>
+        <img
+          src="src/assets/images/dog.gif"
+          alt="dog lover"
+          className="dogImg"
+        />
+        <div className="bottomLine">
+          <div className="quote">
+            Living, learning, & leveling up one day at a time.
+          </div>
+          <div className="socialIcons">
+            <div>
+              <svg
+                className="svg-inline--fa fa-linkedin-in fa-w-14 fa-fw"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="linkedin-in"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                data-fa-i2svg=""
+              >
+                <path
+                  fill="currentColor"
+                  d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+                ></path>
+              </svg>
+            </div>
+            <div>
+              <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                <path d="m75 512h362c41.355469 0 75-33.644531 75-75v-362c0-41.355469-33.644531-75-75-75h-362c-41.355469 0-75 33.644531-75 75v362c0 41.355469 33.644531 75 75 75zm-45-437c0-24.8125 20.1875-45 45-45h362c24.8125 0 45 20.1875 45 45v362c0 24.8125-20.1875 45-45 45h-362c-24.8125 0-45-20.1875-45-45zm0 0" />
+                <path d="m256 391c74.4375 0 135-60.5625 135-135s-60.5625-135-135-135-135 60.5625-135 135 60.5625 135 135 135zm0-240c57.898438 0 105 47.101562 105 105s-47.101562 105-105 105-105-47.101562-105-105 47.101562-105 105-105zm0 0" />
+                <path d="m406 151c24.8125 0 45-20.1875 45-45s-20.1875-45-45-45-45 20.1875-45 45 20.1875 45 45 45zm0-60c8.269531 0 15 6.730469 15 15s-6.730469 15-15 15-15-6.730469-15-15 6.730469-15 15-15zm0 0" />
+              </svg>
+            </div>
+            <div>
+              <svg
+                className="svg-inline--fa fa-envelope fa-w-16 fa-fw"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="far"
+                data-icon="envelope"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                data-fa-i2svg=""
+              >
+                <path
+                  fill="currentColor"
+                  d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"
+                ></path>
+              </svg>
+            </div>
+          </div>
+          <div className="copyright">
+            Handcrafted by me. No
+            <span className="copyRightIcon">
+              <svg
+                height="14px"
+                width="14px"
+                className="svg-inline--fa fa-copyright fa-w-16"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="far"
+                data-icon="copyright"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                data-fa-i2svg=""
+              >
+                <path
+                  fill="currentColor"
+                  d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 448c-110.532 0-200-89.451-200-200 0-110.531 89.451-200 200-200 110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200zm107.351-101.064c-9.614 9.712-45.53 41.396-104.065 41.396-82.43 0-140.484-61.425-140.484-141.567 0-79.152 60.275-139.401 139.762-139.401 55.531 0 88.738 26.62 97.593 34.779a11.965 11.965 0 0 1 1.936 15.322l-18.155 28.113c-3.841 5.95-11.966 7.282-17.499 2.921-8.595-6.776-31.814-22.538-61.708-22.538-48.303 0-77.916 35.33-77.916 80.082 0 41.589 26.888 83.692 78.277 83.692 32.657 0 56.843-19.039 65.726-27.225 5.27-4.857 13.596-4.039 17.82 1.738l19.865 27.17a11.947 11.947 0 0 1-1.152 15.518z"
+                ></path>
+              </svg>
+              <i className="far fa-copyright"></i>
             </span>
-            and ~let's talk.
-          </p>
-          <img src="src/assets/images/dog.gif" alt="dog lover" className="dogImg" />
-          <div>Some stupid change</div>
-          <div className="quote">Living, learning, & leveling up one day at a time.</div>
-        </section>
-
-    )
+            copyright issues &#128522;
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
